04b794d39128f8e90dd01127bbc895132239f58f
ProfL3403 <ProfL3403@lab.aca>
2022-09-23 11:44:39 -0500

    Navegacion terminada

--- app/src/main/java/pe/edu/ulima/itlab/memoriagame/presentation/screen/MainNavScreen.kt
+++ app/src/main/java/pe/edu/ulima/itlab/memoriagame/presentation/screen/MainNavScreen.kt
@@ -2,9 +2,11 @@ package pe.edu.ulima.itlab.memoriagame.presentation.screen
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 @Composable
 fun MainNavScreen(
@@ -16,11 +18,24 @@ fun MainNavScreen(
     ){
         composable("settings") {
             SettingsScreen { numRows, numCols ->
-                navController.navigate("board")
+                navController.navigate("board/${numRows}/${numCols}")
             }
         }
-        composable("board") {
-            BoardScreen(numCols = 4, numRows = 6 )
+        composable(
+            "board/{numRows}/{numCols}",
+            arguments = listOf(
+                navArgument("numRows") {
+                    type = NavType.IntType
+                },
+                navArgument("numCols") {
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            BoardScreen(
+                numCols = it.arguments!!.getInt("numCols"),
+                numRows = it.arguments!!.getInt("numRows")
+            )
         }
     }
 }
